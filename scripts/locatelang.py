3427fdc054bd1ba4c18f9e7a79f4edc353e5fa74
martinhoT <martinho.tavarex@gmail.com>
2023-05-15 15:50:16 +0100

    Documentation for numpy black magic

--- scripts/locatelang.py
+++ scripts/locatelang.py
@@ -32,19 +32,19 @@ minimum_references = np.argmin(data_mtx, axis=0)
 
 minimums_to_consider = np.min(data_mtx, axis=0) < global_threshold
 
-minimum_references[~minimums_to_consider] = -1
+minimum_references[~minimums_to_consider] = -1                                  # -1 -1 -1  9  9  9  9 -1 -1 -1 -1 -1  7  7 -1 -1   base
 
-# nan nan nan 9 9 9 9 nan nan nan nan nan 7 7 nan nan
+prev = np.arange(minimum_references.size)                                       #  0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15   generate indices
+prev[~minimums_to_consider] = -1                                                # -1 -1 -1  3  4  5  6 -1 -1 -1 -1 -1 12 13 -1 -1   set invalid indices to small number
+prev = np.maximum.accumulate(prev)                                              # -1 -1 -1  3  4  5  6  6  6  6  6  6 12 13 13 13   accumulate maximum from left to right
 
-prev = np.arange(minimum_references.size)
-prev[~minimums_to_consider] = -1
-prev = np.maximum.accumulate(prev)
+filled = minimum_references[prev]                                               #  ?  ?  ?  9  9  9  9  9  9  9  9  9  7  7  7  7   index 'base' with indices from 'prev'
+first_non_minus1 = np.argwhere(minimum_references != -1)[0][0]                  #           ^- index 3                              get the first valid index
+filled[:first_non_minus1] = filled[first_non_minus1]                            #  9  9  9  9  9  9  9  9  9  9  9  9  7  7  7  7   set until valid index (3) the value in valid index (9)
 
-filled = minimum_references[prev]
-first_non_minus1 = np.argwhere(minimum_references != -1)[0][0]
-filled[:first_non_minus1] = filled[first_non_minus1]
-
-sections = np.hstack(([0], np.argwhere(np.ediff1d(filled) != 0).flatten() + 1))
+ediff = np.ediff1d(filled)                                                      #  0  0  0  0  0  0  0  0  0  0  0 -2  0  0  0      check the places with discontinuity (reference change)
+all_but_first = np.argwhere(ediff != 0).flatten() + 1                           # [11] + 1 = [12]                                   get the index of those discontinuity places (sum 1 since it's shifted to the left)
+sections = np.hstack(([0], all_but_first))                                      # [0] + [12] = [0, 12]                              add the first section (starts at the beginning)
 
 print(sections)
 print(filled[sections])
