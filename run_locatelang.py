7259df33289236c46ed24a9fbdcee798d10452f1
martinhoT <martinho.tavarex@gmail.com>
2023-05-28 20:43:38 +0100

    Support for locatelang arguments on run_locatelang, some results

--- run_locatelang.py
+++ run_locatelang.py
@@ -1,5 +1,6 @@
 import subprocess
 import shutil
+import numpy as np
 
 lista = [
 #    '-p f',
@@ -14,19 +15,25 @@ lista = [
 #    '-p c:1:3',
 #    '-p c:10:3',
 #    '-p c:100:3',
-    '-p c:1:3',
-    '-p c:1:6',
+#    '-p c:1:3',
+#    '-p c:1:6',
 #    '-p c:1:9',
 ]
 
-file_target="all_languages_random"
+lista_ll = [
+    ('-m {}', np.arange(1.0, 0.79, -0.01)),
+    ('-s {}', np.arange(1.0, 2.05, 0.05)),
+    ('-f {}', np.arange(100, 2100, 100)),
+]
+
+file_target = "all_languages"
 
 command = ['python3', 'scripts/locatelang.py', '-t', 'example/target/'+file_target+".txt", '-p', '6', '-f', '1000','--plot', '--save-result']
 
-try:
-    shutil.rmtree("scripts/input/"+file_target)
-except:
-    pass
+# try:
+#     shutil.rmtree("scripts/input/"+file_target)
+# except:
+#     pass
 
 for arg in lista:
     out=arg.replace(' ', '_')
@@ -39,7 +46,7 @@ for arg in lista:
     for i in arg.split(' '):
         tmp.append(i)
 
-    result = subprocess.run(tmp, capture_output=True, text=True)
+    result = subprocess.run(tmp, capture_output=True, text=True, input='y')
 
     if result.returncode == 0:
         print("success:" + str(tmp))
@@ -50,5 +57,32 @@ for arg in lista:
         print(result.stderr)
         print(result.stdout)
 
-    shutil.rmtree("scripts/input/"+file_target)
+    # shutil.rmtree("scripts/input/"+file_target)
 
+
+command = ['python3', 'scripts/locatelang.py', '-t', 'example/target/' + file_target + ".txt", '-p', '12']
+cpm_args = ['--', '-p', 'c:10:3', '-t', 'c:0.01']
+evaluate_command = ['python3', 'scripts/evaluate.py', 'example/target/' + file_target + ".txt"]
+
+for arg_fmt, values in lista_ll:
+    out = arg_fmt.split(' ')[0]
+    with open('results/ll' + out + '.txt', 'wt') as f:
+    
+        for value in values:
+            print("Running: " + arg_fmt.format(value))
+
+            tmp = command.copy()
+            tmp.append(arg_fmt.format(value))
+            tmp.extend(cpm_args)
+
+            locate_lang_process = subprocess.run(tmp, capture_output=True, input=b'y')
+            evaluate_process = subprocess.run(evaluate_command, capture_output=True, input=locate_lang_process.stdout)
+
+            if evaluate_process.returncode == 0 and locate_lang_process.returncode == 0:
+                print("success:" + str(tmp))
+                f.write(evaluate_process.stdout.decode())
+
+            else:
+                print("failure: " + str(tmp))
+                print(locate_lang_process.stderr.decode())
+                print(evaluate_process.stderr.decode())
